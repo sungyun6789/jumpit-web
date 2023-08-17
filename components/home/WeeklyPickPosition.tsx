5351974ef0dd836555495854f974fccbd8f8db1b
sungyun6789 <sungyun5423@gmail.com>
2023-08-17 23:32:21 +0900

    style: 이번주 인기 포지션 모바일 대응

--- components/home/WeeklyPickPosition.tsx
+++ components/home/WeeklyPickPosition.tsx
@@ -9,7 +9,10 @@ const WeeklyPickPosition = () => {
   return (
     <Block>
       <TitleBox>
-        <Title>프론트엔드 개발자 이번주 인기 포지션</Title>
+        <Title>
+          <BreakTag>프론트엔드 개발자 </BreakTag>
+          이번주 인기 포지션
+        </Title>
         <ViewAllLink href="/positions">전체 보기</ViewAllLink>
       </TitleBox>
 
@@ -23,6 +26,10 @@ export default WeeklyPickPosition;
 const Block = styled.section`
   margin: 100px auto 0px;
   max-width: 1080px;
+
+  @media (max-width: 1080px) {
+    margin-top: 40px;
+  }
 `;
 
 const TitleBox = styled.div`
@@ -30,11 +37,38 @@ const TitleBox = styled.div`
   justify-content: space-between;
   align-items: center;
   padding: 0 10px;
+
+  @media (max-width: 1080px) {
+    padding: 0 16px;
+    margin-bottom: 6px;
+  }
+
+  @media (max-width: 600px) {
+    align-items: end;
+  }
 `;
 
 const Title = styled.h1`
   font-size: 24px;
   line-height: 40px;
+
+  @media (max-width: 600px) {
+    white-space: pre;
+    font-size: 20px;
+    line-height: 32px;
+  }
+`;
+
+const BreakTag = styled.span`
+  font-size: 24px;
+  line-height: 40px;
+
+  @media (max-width: 600px) {
+    display: block;
+    white-space: pre;
+    font-size: 20px;
+    line-height: 32px;
+  }
 `;
 
 const ViewAllLink = styled(Link)`
@@ -43,4 +77,13 @@ const ViewAllLink = styled(Link)`
   letter-spacing: -0.5px;
   color: #888888;
   text-decoration: underline;
+
+  @media (max-width: 1080px) {
+    text-decoration: none;
+  }
+
+  @media (max-width: 600px) {
+    font-size: 14px;
+    padding: 8px 4px 7px 0px;
+  }
 `;
