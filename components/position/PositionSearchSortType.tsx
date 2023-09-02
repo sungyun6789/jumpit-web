13807e875789562854f18a9591785e68aa19ccdd
sungyun6789 <sungyun5423@gmail.com>
2023-09-02 22:52:25 +0900

    feat: 포지션 검색 순서 선택하는 컴포넌트 구현

---
+++ components/position/PositionSearchSortType.tsx
@@ -0,0 +1,67 @@
+import styled from '@emotion/styled';
+import { useRouter } from 'next/router';
+
+import Button from '../common/Button';
+
+type OptionType = 'rsp_rate' | 'reg_dt' | 'popular';
+
+interface SearchTypeOptionModel {
+  label: string;
+  value: OptionType;
+}
+
+const SEARCH_TYPE_OPTIONS: SearchTypeOptionModel[] = [
+  { label: '응답률순', value: 'rsp_rate' },
+  { label: '최신순', value: 'reg_dt' },
+  { label: '인기순', value: 'popular' },
+];
+
+const PositionSearchSortType = () => {
+  const { pathname, query, push } = useRouter();
+  const selectedOption = query.sort ?? 'rsp_rate';
+
+  const onClickOption = (option: OptionType) => {
+    push({
+      pathname,
+      query: {
+        ...query,
+        sort: option,
+      },
+    });
+  };
+
+  return (
+    <Block>
+      {SEARCH_TYPE_OPTIONS.map((option) => (
+        <Option
+          key={option.label}
+          isSelected={option.value === selectedOption}
+          onClick={() => onClickOption(option.value)}
+        >
+          {option.label}
+        </Option>
+      ))}
+    </Block>
+  );
+};
+
+export default PositionSearchSortType;
+
+const Block = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  gap: 20px;
+  width: 1060px;
+  margin: auto;
+  padding: 36px 0 6px 0;
+`;
+
+const Option = styled(Button)<{ isSelected: boolean }>`
+  color: #222222;
+  font-size: 14px;
+
+  ${(props) =>
+    props.isSelected && {
+      fontWeight: 700,
+    }}
+`;
--- pages/positions/index.tsx
+++ pages/positions/index.tsx
@@ -1,5 +1,6 @@
 import PositionLayout from '~/components/layout/PositionLayout';
 import PositionSearchList from '~/components/position/PositionSearchList';
+import PositionSearchSortType from '~/components/position/PositionSearchSortType';
 import PositionTag from '~/components/position/PositionTag';
 
 const PositionsPage = () => {
@@ -8,6 +9,7 @@ const PositionsPage = () => {
       <PositionTag title="직무 탐색" />
       <PositionLayout>
         <PositionSearchList />
+        <PositionSearchSortType />
       </PositionLayout>
     </>
   );
