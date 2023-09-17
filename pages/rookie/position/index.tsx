929f6b6e0c5894fe42f4a65f61b7064222e5e76b
sungyun6789 <sungyun5423@gmail.com>
2023-09-17 21:19:30 +0900

    feat: 절찬 채용중 페이지 접근 시 curation 쿼리 기본 값 추가

--- pages/rookie/position/index.tsx
+++ pages/rookie/position/index.tsx
@@ -1,4 +1,18 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
 const RookiePositionPage = () => {
+  const { pathname, push } = useRouter();
+
+  useEffect(() => {
+    push({
+      pathname,
+      query: {
+        curation: undefined,
+      },
+    });
+  }, []);
+
   return <div>RookiePositionPage</div>;
 };
 
