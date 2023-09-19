f6aa46ab686f931dd64cbf025f68a5fbe9b13caa
sungyun6789 <sungyun5423@gmail.com>
2023-09-20 00:21:48 +0900

    feat: 특정 페이지에서 일부 레이아웃 렌더링 되지 않도록 조건 설정

--- components/layout/BasicLayout.tsx
+++ components/layout/BasicLayout.tsx
@@ -1,21 +1,27 @@
 import styled from '@emotion/styled';
+import { useRouter } from 'next/router';
 
 import Footer from '../base/Footer';
 import Header from '../base/Header';
 
 import AppDownload from './AppDownload';
 
+/** footer가 없는 페이지 목록 */
+const FOOTER_FILTER_PATH = ['/rookie/position'];
+
 interface Props {
   children: React.ReactNode;
 }
 
 const BasicLayout = ({ children }: Props) => {
+  const { pathname } = useRouter();
+
   return (
     <>
       <Header />
       <Block>{children}</Block>
       <AppDownload />
-      <Footer />
+      {!FOOTER_FILTER_PATH.includes(pathname) && <Footer />}
     </>
   );
 };
@@ -27,4 +33,5 @@ const Block = styled.main`
   overflow-x: hidden;
   margin: auto;
   padding-top: 120px;
+  height: 100%;
 `;
