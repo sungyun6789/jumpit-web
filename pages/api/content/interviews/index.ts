381f905e95e47db0937ee1199549e69792ea1656
sungyun6789 <sungyun5423@gmail.com>
2023-09-27 04:05:46 +0900

    feat: 인터뷰 목록 조회하는 jumpit api 구현

---
+++ pages/api/content/interviews/index.ts
@@ -0,0 +1,48 @@
+import axios from 'axios';
+
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export interface InterviewContent {
+  companyLogo: string;
+  companyName: string;
+  contentType: string;
+  id: number;
+  imagePath: string;
+  jobCategories: {
+    id: number;
+    name: string;
+  }[];
+  readingTime: number;
+  title: string;
+  viewCnt: number;
+}
+
+export interface InterviewListResponse {
+  code: string;
+  message: string;
+  result: {
+    contents: InterviewContent[];
+    page: number;
+    totalCount: number;
+  };
+  status: number;
+}
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    if (req.method === 'GET') {
+      const { data } = await axios.get<InterviewListResponse>('https://api.jumpit.co.kr/api/content/interviews', {
+        params: {
+          page: req.query.page,
+          size: req.query.size,
+        },
+      });
+      return res.json(data);
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500);
+  }
+};
+
+export default handler;
