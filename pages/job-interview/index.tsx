30c5da4137fbda1ce3f6516473c264b346595e57
sungyun6789 <sungyun5423@gmail.com>
2023-09-29 23:09:31 +0900

    feat: 일정 높이만큼 스크롤 시 최상위로 이동하는 버튼 컴포넌트 구현

--- pages/job-interview/index.tsx
+++ pages/job-interview/index.tsx
@@ -1,11 +1,13 @@
 import JobInterviewCategory from '~/components/interview/JobInterviewCategory';
 import JobInterviewList from '~/components/interview/JobInterviewList';
+import JobInterviewScrollTopButton from '~/components/interview/JobInterviewScrollTopButton';
 
 const JobInterViewPage = () => {
   return (
     <>
       <JobInterviewCategory />
       <JobInterviewList />
+      <JobInterviewScrollTopButton />
     </>
   );
 };
