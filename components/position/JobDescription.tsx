6068979f83257248d32a8cb68645d1e0b1e975dc
sungyun6789 <sungyun5423@gmail.com>
2023-10-04 01:37:37 +0900

    feat: 직무 상세 두 번째 섹션 구현 (2/4)

--- components/position/JobDescription.tsx
+++ components/position/JobDescription.tsx
@@ -1,8 +1,10 @@
 import styled from '@emotion/styled';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
+import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { mont, noto } from '~/pages/_app';
 
 import type { PositionResponse } from '~/pages/api/position/[id]';
 
@@ -34,6 +36,58 @@ const JobDescription = () => {
           ))}
         </Tags>
       </Section1>
+
+      <Section2>
+        <dl>
+          <Section2Title>기술스택</Section2Title>
+          <dd>
+            {/* 실서비스와 높이가 달라서 임의로 높이 지정 */}
+            <Section2Pre style={{ height: '78px' }}>
+              {data.techStacks.map((value) => (
+                <TechStack key={value.stack} className={mont.className}>
+                  <TechStackImage src={value.imagePath} width={20} height={20} alt="tech stack" />
+                  {value.stack}
+                </TechStack>
+              ))}
+            </Section2Pre>
+          </dd>
+        </dl>
+
+        <dl>
+          <Section2Title>주요업무</Section2Title>
+          <dd>
+            <Section2Pre className={noto.className}>{data.responsibility}</Section2Pre>
+          </dd>
+        </dl>
+
+        <dl>
+          <Section2Title>자격요건</Section2Title>
+          <dd>
+            <Section2Pre className={noto.className}>{data.qualifications}</Section2Pre>
+          </dd>
+        </dl>
+
+        <dl>
+          <Section2Title>우대사항</Section2Title>
+          <dd>
+            <Section2Pre className={noto.className}>{data.preferredRequirements}</Section2Pre>
+          </dd>
+        </dl>
+
+        <dl>
+          <Section2Title>복지 및 혜택</Section2Title>
+          <dd>
+            <Section2Pre className={noto.className}>{data.welfares}</Section2Pre>
+          </dd>
+        </dl>
+
+        <dl>
+          <Section2Title>채용절차 및 기타 지원 유의사항</Section2Title>
+          <dd>
+            <Section2Pre className={noto.className}>{data.recruitProcess}</Section2Pre>
+          </dd>
+        </dl>
+      </Section2>
     </Block>
   ) : null;
 };
@@ -82,4 +136,50 @@ const Tag = styled(Link)`
   font-weight: 500;
   font-size: 13px;
   line-height: 20px;
+`;
+
+const Section2 = styled.section`
+  margin-top: 48px;
+
+  dl {
+    margin-bottom: 56px;
+  }
+`;
+
+const Section2Title = styled.dt`
+  padding-bottom: 12px;
+  font-size: 18px;
+  color: #000;
+  line-height: 32px;
+  letter-spacing: -0.5px;
+  font-weight: bold;
+`;
+
+const Section2Pre = styled.pre`
+  white-space: pre-line;
+  word-break: break-all;
+  font-size: 16px;
+  color: #444444;
+  line-height: 28px;
+  letter-spacing: -0.5px;
+`;
+
+const TechStackImage = styled(Image)`
+  position: absolute;
+  left: 8px;
+  top: 6px;
+`;
+
+const TechStack = styled.div`
+  display: inline-block;
+  position: relative;
+  margin: 0 6px 6px 0;
+  font-size: 12px;
+  font-weight: 500;
+  line-height: 32px;
+  letter-spacing: 0;
+  color: #000;
+  padding: 0 12px 0 34px;
+  background-color: #f4f4f4;
+  border-radius: 4px;
 `;
