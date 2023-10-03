b764065a5409cc61e44ffbfa4f14285aceabf27b
sungyun6789 <sungyun5423@gmail.com>
2023-10-04 00:43:23 +0900

    feat: 직무 상세 첫 번째 섹션 구현 (1/4)

--- components/position/JobDescription.tsx
+++ components/position/JobDescription.tsx
@@ -1,5 +1,7 @@
+import styled from '@emotion/styled';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 import type { PositionResponse } from '~/pages/api/position/[id]';
@@ -17,7 +19,67 @@ const JobDescription = () => {
     { enabled: isReady }
   );
 
-  return <div>JobDescription</div>;
+  return data ? (
+    <Block>
+      <Section1>
+        <Title>{data.title}</Title>
+        <CompanyNameLayout>
+          <CompanyName href="/">{data.companyName}</CompanyName>
+        </CompanyNameLayout>
+        <Tags>
+          {data.tags.map((tag) => (
+            <li key={tag.id}>
+              <Tag href={'/positions?tag=' + tag.id}>#{tag.name}</Tag>
+            </li>
+          ))}
+        </Tags>
+      </Section1>
+    </Block>
+  ) : null;
 };
 
 export default JobDescription;
+
+const Block = styled.div`
+  position: relative;
+  width: 630px;
+`;
+
+const Section1 = styled.section`
+  border-bottom: 1px solid #eaeaea;
+`;
+
+const Title = styled.h1`
+  font-weight: bold;
+  font-size: 40px;
+  margin-bottom: 16px;
+  line-height: 1.25;
+`;
+
+const CompanyNameLayout = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 24px;
+`;
+
+const CompanyName = styled(Link)`
+  font-size: 16px;
+`;
+
+const Tags = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  margin-bottom: 16px;
+`;
+
+const Tag = styled(Link)`
+  display: inline-block;
+  margin: 0 6px 6px 0;
+  padding: 5px 15px;
+  border: 1px solid #e4e4e4;
+  border-radius: 100px;
+  color: #444444;
+  font-weight: 500;
+  font-size: 13px;
+  line-height: 20px;
+`;
