647da1dc9c06acf7db677091cdea6ac24282dbfe
sungyun6789 <sungyun5423@gmail.com>
2023-10-03 23:53:07 +0900

    feat: 직무 정보 가져오는 jumpit api 연동

---
+++ pages/api/position/[id].ts
@@ -0,0 +1,102 @@
+import axios from 'axios';
+
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export interface PositionResponse {
+  code: string;
+  message: string;
+  result: {
+    alwaysOpen: boolean;
+    applied: boolean;
+    cacheCompanyImages: {
+      imagePath: string;
+      sortNumber: number;
+    }[];
+    celebration: number;
+    closedAt: string;
+    companyName: string;
+    companyProfileId: number;
+    companyUrl: string;
+    developerInterviews: unknown[];
+    developerInterviewsCount: number;
+    draft: boolean;
+    education: number;
+    follow: boolean;
+    graduate: boolean;
+    id: number;
+    itCompanyStory: unknown[];
+    jobPostingForSearchEngine: {
+      url: string;
+      title: string;
+      employmentType: string[];
+      experienceRequirements: string[];
+      description: string;
+      hiringOrganization: {
+        name: string;
+        logo: string;
+        '@type': string;
+      };
+      identifier: {
+        propertyID: string;
+        value: string;
+        '@type': string;
+      };
+      jobLocation: {
+        address: {
+          addressRegion: string;
+          addressCountry: string;
+          streetAddress: string;
+          '@type': string;
+        };
+        '@type': string;
+      };
+      '@context': string;
+      '@type': string;
+      '@datePosted': string;
+      '@validThrough': string;
+    };
+    location: null;
+    logo: string;
+    maxCareer: number;
+    minCareer: number;
+    newcomer: boolean;
+    positionStatus: 'CHECKED';
+    preferredRequirements: string;
+    publishedAt: string;
+    qualifications: string;
+    recruitProcess: string;
+    responsibility: string;
+    scrap: boolean;
+    serviceInfo: string;
+    tags: {
+      id: string;
+      name: string;
+      emoticon: string;
+    }[];
+    techStacks: {
+      stack: string;
+      imagePath: string;
+    }[];
+    title: string;
+    welfares: string;
+    workingPlaces: {
+      address: string;
+      isDomestic: boolean;
+    }[];
+  };
+  status: number;
+}
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    if (req.method === 'GET') {
+      const { data } = await axios.get<PositionResponse>(`https://api.jumpit.co.kr/api/position/${req.query.id}`);
+      return res.json(data);
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500);
+  }
+};
+
+export default handler;
