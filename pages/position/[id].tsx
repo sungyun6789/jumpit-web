646ba6987c6be21d4b6f4084444daea4a672a02f
sungyun6789 <sungyun5423@gmail.com>
2023-10-08 00:21:56 +0900

    docs: 직무 상세 페이지 설명 추가

--- pages/position/[id].tsx
+++ pages/position/[id].tsx
@@ -3,6 +3,10 @@ import JobDescription from '~/components/position/JobDescription';
 import JobDescriptionTopBanner from '~/components/position/JobDescriptionTopBanner';
 import JobDescriptionProvier from '~/context/JobDescriptionProvider';
 
+/**
+ * @description
+ * 실제 서비스에서는 클릭수를 업데이트하는 count-up api가 존재하는데 여기서는 별도로 구현하지 않음
+ */
 const PositionPage = () => {
   return (
     <JobDescriptionProvier>
