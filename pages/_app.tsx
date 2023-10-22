c8ce97776fe8e0d23bb07e53e70193cafc26291f
sungyun6789 <sungyun5423@gmail.com>
2023-10-22 14:13:15 +0900

    env: 파이어베이스 환경 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,8 @@ import 'slick-carousel/slick/slick-theme.css';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 import { Noto_Sans_KR, Montserrat } from 'next/font/google';
 import BasicLayout from '~/components/layout/BasicLayout';
 
@@ -26,6 +28,19 @@ const queryClient = new QueryClient({
     },
   },
 });
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
