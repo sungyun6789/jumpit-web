8351a036349b8f17d8ef985424aca9be622dbe3e
sungyun6789 <sungyun5423@gmail.com>
2023-11-10 22:40:22 +0900

    feat: 회사 상세 페이지 생성

---
+++ pages/company/[id]/index.tsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+const CompanyPage = () => {
+  return <Block>회사 상세 페이지</Block>;
+};
+
+export default CompanyPage;
+
+const Block = styled.section`
+  position: relative;
+  padding: 60px 0 80px;
+  max-width: 1060px;
+  margin: 0 auto;
+`;
