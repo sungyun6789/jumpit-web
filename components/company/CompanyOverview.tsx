0dc3f20532b66ef4a6f787964ff2cd6acc7d4012
sungyun6789 <sungyun5423@gmail.com>
2023-11-15 23:36:53 +0900

    feat: 회사 소개 이미지에 뷰어 적용

--- components/company/CompanyOverview.tsx
+++ components/company/CompanyOverview.tsx
@@ -1,39 +1,86 @@
 import styled from '@emotion/styled';
 import Image from 'next/image';
 import { useContext, useState } from 'react';
+import ImageViewer from 'react-simple-image-viewer';
 import { CompanyInfoContext } from '~/context/CompanyInfoProvider';
 
+/** 뷰어를 사용하지 않고 처음 화면에 보여줄 이미지 목록 길이 */
+const IMAGE_LENGTH = 3;
+
 const CompanyOverview = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isDescriptionOpen, setIsDescriptionOpen] = useState(false);
+  const [isViewerOpen, setIsViewerOpen] = useState(false);
+  const [currentImage, setCurrentImage] = useState(0);
+
   const data = useContext(CompanyInfoContext);
+  const images = data?.profileImages.slice(0, IMAGE_LENGTH);
+  const allImages = (data?.profileImages ?? []).map((profile) => profile.imagePath);
+
+  const openImageViewer = (index: number) => {
+    setCurrentImage(index);
+    setIsViewerOpen(true);
+  };
+
+  const closeImageViewer = () => {
+    setCurrentImage(0);
+    setIsViewerOpen(false);
+  };
 
   return (
-    <Block>
-      <Title>회사 소개</Title>
+    <>
+      <Block>
+        <Title>회사 소개</Title>
 
-      <Content open={isOpen}>{data?.companyService.description}</Content>
+        <Content open={isDescriptionOpen}>{data?.companyService.description}</Content>
 
-      <ButtonWrap>
-        <OpacityBox open={isOpen} />
-        <Button onClick={() => setIsOpen(!isOpen)}>
-          기업/서비스 소개 더보기
-          <Icon
-            open={isOpen}
-            src="https://www.jumpit.co.kr/assets/images/arrow-down-black.svg"
-            width={16}
-            height={16}
-            alt="아래쪽 화살표"
+        <ButtonWrap>
+          <OpacityBox open={isDescriptionOpen} />
+          <Button onClick={() => setIsDescriptionOpen(!isDescriptionOpen)}>
+            기업/서비스 소개 더보기
+            <Icon
+              open={isDescriptionOpen}
+              src="https://www.jumpit.co.kr/assets/images/arrow-down-black.svg"
+              width={16}
+              height={16}
+              alt="아래쪽 화살표"
+            />
+          </Button>
+        </ButtonWrap>
+
+        <CompanyImageWrap>
+          {images?.map((profile, index) => (
+            <PositionBlock key={profile.imagePath} onClick={() => openImageViewer(index)}>
+              <CompanyImage src={profile.imagePath} width={202} height={134} alt="회사 이미지" />
+              {index + 1 === IMAGE_LENGTH && <MoreCnt>+ {allImages.length - images.length}</MoreCnt>}
+            </PositionBlock>
+          ))}
+        </CompanyImageWrap>
+      </Block>
+
+      {isViewerOpen && (
+        <Viewer>
+          <ImageViewer
+            src={allImages}
+            currentIndex={currentImage}
+            disableScroll={false}
+            closeOnClickOutside={true}
+            onClose={closeImageViewer}
+            leftArrowComponent={
+              <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="none" viewBox="0 0 64 64">
+                <circle cx="32" cy="32" r="32" fill="#fff" opacity="0.9"></circle>
+                <path stroke="#222" strokeWidth="2" d="M35.334 21.333 24.667 32l10.667 10.667"></path>
+              </svg>
+            }
+            rightArrowComponent={
+              <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="none" viewBox="0 0 64 64">
+                <circle cx="32" cy="32" r="32" fill="#fff" opacity="0.9"></circle>
+                <path stroke="#222" strokeWidth="2" d="M28.666 42.667 39.333 32 28.666 21.333"></path>
+              </svg>
+            }
           />
-        </Button>
-      </ButtonWrap>
-
-      {/* @todo 이미지 클릭시 뷰어 띄우기 */}
-      <CompanyImageWrap>
-        {data?.profileImages.map((profile) => (
-          <CompanyImage key={profile.imagePath} src={profile.imagePath} width={202} height={134} alt="회사 이미지" />
-        ))}
-      </CompanyImageWrap>
-    </Block>
+        </Viewer>
+      )}
+    </>
   );
 };
 
@@ -111,5 +158,50 @@ const CompanyImageWrap = styled.div`
 `;
 
 const CompanyImage = styled(Image)`
+  object-fit: cover;
   border-radius: 4px;
+  cursor: pointer;
+`;
+
+const PositionBlock = styled.div`
+  position: relative;
+`;
+
+const MoreCnt = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: rgba(0, 0, 0, 0.6);
+  border-radius: 4px;
+  font-weight: 500;
+  font-size: 22px;
+  line-height: 26px;
+  letter-spacing: -0.5px;
+  color: #fff;
+  cursor: pointer;
+`;
+
+const Viewer = styled.div`
+  .react-simple-image-viewer__modal {
+    z-index: 2;
+    background-color: rgba(0, 0, 0, 0.6);
+  }
+
+  .react-simple-image-viewer__close {
+    font-weight: 400;
+    opacity: unset;
+  }
+
+  .react-simple-image-viewer__previous {
+    opacity: unset;
+  }
+
+  .react-simple-image-viewer__next {
+    opacity: unset;
+  }
 `;
