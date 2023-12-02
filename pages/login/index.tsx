ca32637fc8ab719c9f647ed93044e8f0b1a4d87f
sungyun6789 <sungyun5423@gmail.com>
2023-12-03 00:21:51 +0900

    feat: 페이지 타이틀 추가

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,7 +1,15 @@
+import Head from 'next/head';
 import Login from '~/components/login/Login';
 
 const LoginPage = () => {
-  return <Login />;
+  return (
+    <>
+      <Head>
+        <title>점핏 | 로그인</title>
+      </Head>
+      <Login />
+    </>
+  );
 };
 
 export default LoginPage;
