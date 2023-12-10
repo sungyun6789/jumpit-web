e91a94ef2457a4d61bb6c8d3356fec38bacefd3c
sungyun6789 <sungyun5423@gmail.com>
2023-12-11 03:20:45 +0900

    feat: 테마 페이지 타이틀 추가

--- pages/theme/index.tsx
+++ pages/theme/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import ScrollTopButton from '~/components/common/ScrollTopButton';
 import ThemeZipCardList from '~/components/theme/ThemeZipCardList';
 import ThemeZipTag from '~/components/theme/ThemeZipTag';
@@ -6,6 +7,9 @@ import ThemeZipTitle from '~/components/theme/ThemeZipTitle';
 const ThemePage = () => {
   return (
     <>
+      <Head>
+        <title>점핏 | 테마별 모음.zip</title>
+      </Head>
       <ThemeZipTitle />
       <ThemeZipCardList />
       <ThemeZipTag />
