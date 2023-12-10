e603ead53d553459469969e8c48977ac0b8b4cbd
sungyun6789 <sungyun5423@gmail.com>
2023-12-10 23:41:08 +0900

    feat: 이력서 페이지 임시 가드 추가

--- pages/resumes/index.tsx
+++ pages/resumes/index.tsx
@@ -1,4 +1,17 @@
+import { useRouter } from 'next/router';
+import { useSession } from 'next-auth/react';
+import { useEffect } from 'react';
+
 const ResumesPage = () => {
+  const router = useRouter();
+  const { data } = useSession();
+
+  useEffect(() => {
+    if (!data?.user) {
+      router.replace('/login');
+    }
+  }, [data]);
+
   return <div>이력서 페이지</div>;
 };
 
