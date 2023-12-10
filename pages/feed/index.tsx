ce442e09d6436032421df1665243affaa5e91c09
sungyun6789 <sungyun5423@gmail.com>
2023-12-10 13:31:57 +0900

    feat: 꿀 피드 페이지 타이틀 추가

--- pages/feed/index.tsx
+++ pages/feed/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import ScrollTopButton from '~/components/common/ScrollTopButton';
 import FeedCardList from '~/components/feed/FeedCardList';
 import FeedTagList from '~/components/feed/FeedTagList';
@@ -5,6 +6,9 @@ import FeedTagList from '~/components/feed/FeedTagList';
 const FeedPage = () => {
   return (
     <div>
+      <Head>
+        <title>점핏 | 꿀 피드</title>
+      </Head>
       <FeedTagList />
       <FeedCardList />
       <ScrollTopButton right={-80} />
