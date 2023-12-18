f3c67c8c98956620c8399773fbbe52230a323954
sungyun6789 <sungyun5423@gmail.com>
2023-12-18 22:57:19 +0900

    feat: qna 조회 옵션 선택 컴포넌트

---
+++ components/career-qna/CareerqnaListCategoryControl.tsx
@@ -0,0 +1,84 @@
+import styled from '@emotion/styled';
+import { useRouter } from 'next/router';
+import COLORS from '~/constants/colors';
+
+const OPTIONS = ['💻 전체', '💼 커리어', '👔 취업', '🚀 기술스택'];
+
+const CareerqnaListCategoryControl = () => {
+  const router = useRouter();
+  const category = Number(router.query.category ?? 1);
+
+  const onClick = (value: number) => {
+    return router.push({
+      pathname: router.pathname,
+      query: {
+        ...router.query,
+        category: value + 1,
+      },
+    });
+  };
+
+  return (
+    <Block>
+      <Question>
+        <Button>질문하기</Button>
+      </Question>
+
+      <OptionList>
+        {OPTIONS.map((option, index) => (
+          <Option key={option} isSelected={index + 1 == category} onClick={() => onClick(index)}>
+            {option}
+          </Option>
+        ))}
+      </OptionList>
+    </Block>
+  );
+};
+
+export default CareerqnaListCategoryControl;
+
+const Block = styled.section`
+  width: 240px;
+  height: fit-content;
+  background-color: #fff;
+  border-radius: 12px;
+  box-shadow: rgba(0, 0, 0, 0.06) 0px 4px 36px;
+`;
+
+const Question = styled.div`
+  padding: 24px 24px 20px;
+  border-bottom: 1px solid #f2f2f5;
+`;
+
+const Button = styled.button`
+  padding: 0 16px;
+  font-size: 15px;
+  font-weight: 700;
+  line-height: 24px;
+  height: 48px;
+  border-radius: 8px;
+  color: #fff;
+  width: 100%;
+  background-color: ${COLORS.primary};
+  outline: none;
+
+  :hover {
+    background-color: #01c662;
+  }
+`;
+
+const OptionList = styled.ul`
+  padding: 14px 0;
+`;
+
+const Option = styled.li<{ isSelected: boolean }>`
+  padding: 10px 24px;
+  font-size: 15px;
+  line-height: 24px;
+  cursor: pointer;
+  font-weight: ${(props) => props.isSelected && '700'};
+
+  :hover {
+    text-decoration: underline;
+  }
+`;
---
+++ components/career-qna/CareerqnaListOrderControl.tsx
@@ -0,0 +1,47 @@
+import styled from '@emotion/styled';
+import { useRouter } from 'next/router';
+
+const OPTIONS = [
+  { label: '최신순', value: 'reg_dt' },
+  { label: '공감순', value: 'like' },
+  { label: '조회순', value: 'view' },
+];
+
+const CareerqnaListOrderControl = () => {
+  const router = useRouter();
+  const order = router.query.sort ?? 'reg_dt';
+
+  const onClick = (value: string) => {
+    return router.push({
+      pathname: router.pathname,
+      query: {
+        ...router.query,
+        sort: value,
+      },
+    });
+  };
+
+  return (
+    <Block>
+      {OPTIONS.map((option) => (
+        <Label key={option.value} isSelected={order === option.value} onClick={() => onClick(option.value)}>
+          {option.label}
+        </Label>
+      ))}
+    </Block>
+  );
+};
+
+export default CareerqnaListOrderControl;
+
+const Block = styled.ul`
+  display: flex;
+  gap: 20px;
+`;
+
+const Label = styled.li<{ isSelected: boolean }>`
+  font-size: 16px;
+  cursor: pointer;
+  color: ${(props) => (props.isSelected ? '#000' : '#999')};
+  font-weight: ${(props) => props.isSelected && '700'};
+`;
