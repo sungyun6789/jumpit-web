fc7f96565d3c5cb47a50b023e222dd76497070ed
sungyun6789 <sungyun5423@gmail.com>
2023-12-19 21:23:53 +0900

    feat: seo를 위한 meta 태그 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -33,6 +33,22 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
     <QueryClientProvider client={queryClient}>
       <Head>
         <title>점핏 | 개발자 커리어 점프</title>
+        <meta name="description" content="개발자 커리어 점프, 점핏" />
+        <meta
+          name="keywords"
+          content="개발자 채용,이력서,이력서양식,스타트업 채용,대기업 채용,취업,구인,구직,면접제안,기업정보,커리어,커리어개발,기업정보,채용정보"
+        />
+        <meta name="theme-color" content="#ffffff" />
+        <link rel="canonical" href="https://www.jumpit.co.kr/" />
+        <meta property="og:title" content="점핏" />
+        <meta property="og:description" content="개발자 커리어 점프, 점핏" />
+        <meta property="og:url" content="https://www.jumpit.co.kr/" />
+        <meta property="og:site_name" content="개발자 커리어 점프, 점핏" />
+        <meta property="og:locale" content="ko_KR" />
+        <meta property="og:image" content="https://cdn.jumpit.co.kr/jumpit/jumpit_share.png" />
+        <meta property="og:image:width" content="800" />
+        <meta content="420" property="og:image:height" />
+        <meta content="website" property="og:type" />
       </Head>
       <div>
         <SessionProvider session={session}>
