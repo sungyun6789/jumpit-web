9b56a837247477459b316a8a567c817b1ba50dc4
sungyun6789 <sungyun5423@gmail.com>
2024-01-10 00:45:46 +0900

    fix(auth): 실서버 환경에서 next-auth 함수 호출 안 되는 버그 수정

--- types/global.d.ts
+++ types/global.d.ts
@@ -2,5 +2,6 @@ declare namespace NodeJS {
   interface ProcessEnv {
     readonly GOOGLE_CLIENT_ID: string;
     readonly GOOGLE_CLIENT_SECRET: string;
+    readonly NEXT_AUTH_SECRET: string;
   }
 }
